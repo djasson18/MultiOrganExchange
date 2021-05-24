7d241faf32bbbbe0b8a2e1946fea6142b4afc94d
williamcg <williamcgolub@gmail.com>
2021-05-24 04:00:08 -0400

    Revert "Merge branch 'main' of https://github.com/djasson18/MultiOrganExchange into main"
    This reverts commit da9abeb5872a2f74685808488fed2a19e8c72ec5, reversing
    changes made to 4b92614ccf3cb67ba6f883057c3dec7c1ebdc097.

--- simpleMatching.py
+++ simpleMatching.py
@@ -66,14 +66,13 @@ def pairedMatch(pdList):
         # cycle through pairs of patients trying to find matches
         for pair1 in toMatch:
             for pair2 in toMatch:
-                if isCompatible(pair1[0], pair2[1]) and isCompatible(pair2[0], pair1[1]): #if 1 needs 2's kidney
+                if isCompatible(pair1[0], pair2[1]) and isCompatible(pair2[0], pair1[1]) and pair1 != pair2: #if 1 needs 2's kidney
                     pair1[0].cured = date - pair1[0].date
                     pair2[0].cured = date - pair2[0].date
                     toMatch.remove(pair1)
                     matched.append(pair1)
-                    if pair1 != pair2:
-                        toMatch.remove(pair2)
-                        matched.append(pair2)
+                    toMatch.remove(pair2)
+                    matched.append(pair2)
 
         date += 1
 
